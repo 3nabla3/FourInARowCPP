f7260efd1b31804e68a05ce3c69f5c24189daa84
alban <albanguyon@yahoo.fr>
2022-11-06 15:01:16 -0500

    added build and run scripts

---
+++ setup.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Setup the project with cmake
+echo "Building the compilation environment"
+cmake . -B build -G "Unix Makefiles"
+cd build || exit
+
+# compile the project with make
+if [[ $(make -j "$(nproc)") -ne 0 ]]; then
+  echo "Compilation failed"
+  exit
+fi
+
+# Run the executable
+echo "Running the executable"
+
+chmod +x run.bash
+./run.bash
+
+
+
