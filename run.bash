43f9db3410bcf6f1ba7c3c0ff0d02bb2561fd6c4
alban <albanguyon@yahoo.fr>
2022-11-06 15:12:38 -0500

    small bugs

--- run.bash
+++ run.bash
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+# make sure the latest version is compiled before running
+
+cd build || exit
+make -j "$(nproc)"
+cd ..
+
 ./build/FourInARowCPP
--- setup.bash
+++ setup.bash
@@ -3,18 +3,9 @@
 # Setup the project with cmake
 echo "Building the compilation environment"
 cmake . -B build -G "Unix Makefiles"
-cd build || exit
-
-# compile the project with make
-if [[ $(make -j "$(nproc)") -ne 0 ]]; then
-  echo "Compilation failed"
-  exit
-fi
-cd .. || exit
 
 # Run the executable
 echo "Running the executable"
-
 chmod +x run.bash
 ./run.bash
 
