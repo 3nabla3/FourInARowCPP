d602018280d05fe9622f3acabc94d1973cfb9868
Alban <albanguyon@yahoo.fr>
2022-11-11 19:54:36 -0500

    added testing

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,4 +1,4 @@
-#define BOOST_TEST_MODULE isEq
+#define BOOST_TEST_MODULE myModule
 #include <boost/test/included/unit_test.hpp>
 
 BOOST_AUTO_TEST_CASE(isEq) {
@@ -6,4 +6,12 @@ BOOST_AUTO_TEST_CASE(isEq) {
 	int b = 6;
 	BOOST_CHECK_EQUAL(a + 1, b);
 	BOOST_CHECK_NE(a, b);
+}
+
+BOOST_AUTO_TEST_CASE(isNotEq) {
+	int a = 10;
+	int b = 10;
+	BOOST_CHECK_EQUAL(a, b);
+	BOOST_CHECK_EQUAL(a, 10);
+	BOOST_CHECK_EQUAL(9, b - 1);
 }
